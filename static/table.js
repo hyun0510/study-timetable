24eff4babf01f6bfbbce493e77f30a3db7eff604
hyun0510 <104971202+hyun0510@users.noreply.github.com>
2022-05-05 16:31:27 +0900

    Add files via upload

---
+++ static/table.js
@@ -0,0 +1,36 @@
+var computer = document.getElementById("computer");
+computer.addEventListener('click', function()
+    {
+      alert("컴퓨터기초");
+    });
+var lr = document.getElementById("lr");
+lr.addEventListener('click', function()
+   {
+      alert("communicatio nEnglish(L&R)");
+   });
+var culture = document.getElementById("culture");
+culture.addEventListener('click', function()
+{
+      alert("문화와 기술");
+});
+var cprogramming = document.getElementById("cprogramming");
+cprogramming.addEventListener('click', function()
+{
+      alert("C프로그래밍");
+});
+var writing = document.getElementById("writing");
+writing.addEventListener('click', function()
+{
+      alert("글쓰기 기초");
+});
+var sw = document.getElementById("sw");
+sw.addEventListener('click', function()
+{
+      alert("communication English(S&W)");
+});
+var math = document.getElementById("math");
+math.addEventListener('click', function()
+{
+     alert("확률통계");
+});
+                        
